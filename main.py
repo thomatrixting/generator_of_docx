54201bfc205c624b1c5417bbdec0384b02e1e818
thomas rodrigues <thomas.quamtum@gmail.com>
2021-12-07 11:05:13 -0500

    i maded the first exitose rename

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import numpy as np
 import pandas as pd
 
-TEXT = f'hola {%} esto es una prueba'
+TEXT = f'hola {} esto es una prueba'
--- testing_docx.py
+++ testing_docx.py
@@ -1,7 +1,20 @@
 from docx import Document
 
-doc = Document('word_test.docx')
+doc = Document('templeates/certificate_templeate.docx')
 
+# counter = 0
+# for paragraph in doc.paragraphs: #is the 16 paragraph
+#     print(paragraph.text)
+#     print(counter)
+#     counter += 1
 
-print(doc.paragraphs[0].text)
+text = doc.paragraphs[16].text
+text = text.format('thomas','1031650')
+doc.paragraphs[16].text = text
+
+# print(text)
+print(doc.paragraphs[16].text)
+
+doc.save('files_words/demo.docx')
+
 
