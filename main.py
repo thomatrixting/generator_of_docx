039316254668b542a4f4b9b1935a7ff5f4e3674c
thomas rodrigues <thomas.quamtum@gmail.com>
2021-12-07 11:39:44 -0500

    end a 2 try now with a simulation of the exel and it turns that it works

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import numpy as np
+from traceback import print_tb
+from docx import Document
 import pandas as pd
 
-TEXT = f'hola {} esto es una prueba'
+def run():
+    df = pd.DataFrame({'nombre':['thomas','jeronimo'],'cedula':[2356,2567]})
+    df = df.rename(columns={'nombre':'name','cedula':'id'})
+    
+    for index,row in df.iterrows():
+        doc = Document('templeates/certificate_templeate.docx')
+        text_to_change = doc.paragraphs[16].text 
+        text_to_change = text_to_change.format(row['name'],row['id'])
+        doc.paragraphs[16].text  = text_to_change 
+        doc.save('files_words/demo2.docx')
+        print(doc.paragraphs[16].text)
+
+if __name__ == '__main__':
+    run()
